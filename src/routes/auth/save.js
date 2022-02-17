917a9f0cd74dc0e30e2db978ac6dd8e60012758e
Alex Hojong Lee <alexhojonglee@gmail.com>
2022-02-17 22:52:53 +0900

    fix error with To access the request body use the text/json/arrayBuffer/formData methods, e.g. `body = await request.json()`. See https://github.com/sveltejs/kit/pull/3384 for details

--- src/routes/auth/save.js
+++ src/routes/auth/save.js
@@ -1,21 +1,23 @@
 import * as api from '$lib/api.js';
 import { respond } from './_respond';
 
-export async function post({ body: user, locals }) {
-	if (!locals.user) {
-		return {
-			status: 401
-		};
-	}
+export async function post({ request, locals }) {
+  if (!locals.user) {
+    return {
+      status: 401,
+    };
+  }
 
-	const { token } = locals.user;
-	const body = await api.put(
-		'user',
-		{
-			user // TODO individual properties
-		},
-		token
-	);
+  const user = await request.json();
 
-	return respond(body);
+  const { token } = locals.user;
+  const body = await api.put(
+    'user',
+    {
+      user, // TODO individual properties
+    },
+    token
+  );
+
+  return respond(body);
 }
